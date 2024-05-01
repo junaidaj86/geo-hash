209676273514492c21a53e84b1066388df280522
Junaid Ahmed <junaidaj86@gmail.com>
2024-05-01 10:21:08 +0200

    removing unnecessary code

--- src/main/java/com/zinu/geohash/controller/GeoHashController.java
+++ src/main/java/com/zinu/geohash/controller/GeoHashController.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
